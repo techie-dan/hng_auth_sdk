4a3d7c5af6bedce8e01397cec6c31f68ba0cbc17
Olaleye Daniel Ifeoluwa <danielolaleye064gmail.com@Olaleyes-MacBook-Pro.local>
2025-12-13 10:33:15 +0100

    Able to use the SDK on the example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:hng_firebase_auth/hng_firebase_auth.dart';
-
 import 'firebase_options.dart';
 
 Future<void> main() async {
